8cf39669176b8e47d6017ad309c885b17293f37a
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2019-09-15 14:02:22 -0400

    Add files via upload

--- actions.py
+++ actions.py
@@ -32,20 +32,35 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.executor import CollectingDispatcher
 import pandas as pd
 
-class ActionCSVInfo(Action):
+class ActionFileColumns(Action):
    def name(self) -> Text:
-      return "action_csv_info"
+      return "action_file_columns"
+   def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+      dispatcher.utter_message("Here is the list of column names 1")
+      csv_url = tracker.get_slot('file_name')
+      df=pd.read_csv(csv_url)
+      result = list(df)
+      dispatcher.utter_message("Here is the list of column names 2")
+      for i in range(len(result)):
+          dispatcher.utter_message(result[i]+" ")
+      return []
+      
+
+
+class ActionFileRow(Action):
+   def name(self) -> Text:
+      return "action_file_row"
 
    def run(self,
            dispatcher: CollectingDispatcher,
            tracker: Tracker,
            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-      csv_url = tracker.get_slot('csv_url')
+      csv_url = tracker.get_slot('file_name')
+      csv_row = tracker.get_slot('row_number')
       df=pd.read_csv(csv_url)
       result = list(df)
-      dispatcher.utter_message("Here is the list of column names")
-      for i in range(len(result)):
-          dispatcher.utter_message(result[i]+" ")
-
+      dispatcher.utter_message("Here is column ",csv_row)
+      dispathcer.utter_message(df.iloc[[csv_row]])
       return []
+   
