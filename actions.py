ee79d392afdb6e11ed0eefcc0362e61a96a6f01e
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2019-09-21 11:03:50 -0400

    Add files via upload

--- actions.py
+++ actions.py
@@ -32,7 +32,21 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.executor import CollectingDispatcher
 import pandas as pd
 
+# movie db schema
+movie_schema = {"ratings_small":["userId", "movieId","rating","timestamp"],
+                "credit_small":["cast","crew","id"],
+                "movies_metadata_small":["adult","belongs_to_collection","budget","genres","homepage","id","imdb_id","original_language","original_title",
+                "overview","popularity","poster_path","production_companies","production_countries","release_date","revenue","runtime","spoken_languages",
+                "status	tagline	title","video","vote_average","vote_count"],
+                "keywords_small":["id","keywords"],
+                "links_small":["movieId","imdbId","tmdbId"]}
+                
+
+
+
+
 class ActionFileColumns(Action):
+   """ return the column names for a file """
    def name(self) -> Text:
       return "action_file_columns"
    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
@@ -48,6 +62,7 @@ class ActionFileColumns(Action):
 
 
 class ActionFileRow(Action):
+   """ return the values in a specific row """
    def name(self) -> Text:
       return "action_file_row"
    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
@@ -68,8 +83,8 @@ class ActionFileRow(Action):
       #   dispatcher.utter_message(str(result[csv_row][i]))
       return []
 
-class ActionFileRow(Action):
-   # get the min, max, average for continuous columns
+class ActionFirstNRows(Action):
+   """ return the values of the first n rows """
    def name(self) -> Text:
       return "action_first_n_rows"
    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
@@ -87,8 +102,8 @@ class ActionFileRow(Action):
       return []
 
    
-class ActionFileRow(Action):
-   # get the min, max, average for continuous columns
+class ActionLastNRows(Action):
+   """return the values of the last n rows"""
    def name(self) -> Text:
       return "action_last_n_rows"
    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
@@ -106,3 +121,45 @@ class ActionFileRow(Action):
          one_line_output = ", ".join(str_array)
          dispatcher.utter_message(one_line_output)
       return []
+
+class ActionRankColByOtherCol(Action):
+   """return the list of values from one column ranked according to another column"""
+   def name(self) -> Text:
+      return "action_rank_col_by_other_col"
+   def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+      # give me the top 5 movies by budget
+      # give me the [top_bottom] [row_range] [ranked_col} by [ranking_col]
+      # give me the top 5 French movies by budget
+      # give me the [top_bottom] [row_range] [ranked_col] by [ranking_col] where [condition_col] [condition_operator] [condition_value]
+      # top movies from 2004
+      # [top_bottom] [ranked_col] [by [ranking_col] assumed to be [popularity]] [condition_col][condition_value]
+      # slots:
+      # ranked_col
+      # ranking_col
+      # ascending_descending
+      # top_bottom
+      # condition_col
+      # condition_value
+      # condition_operator
+      ranked_col = tracker.get_slot('ranked_col')
+      ranking_col = tracker.get_slot('ranking_col')
+      ascending_descending = tracker.get_slot('ascending_descending')
+      top_bottom = tracker.get_slot('top_bottom')
+      row_range = int(tracker.get_slot('row_range'))
+      condition_col = tracker.get_slot('condition_col')
+      condition_value = tracker.get_slot('condition_value')
+      condition_operator = tracker.get_slot('condition_operator')
+
+      # def map_cols_to_tables(col_dict):
+      #     '''return the tables / files associated with each of the columns'''
+      #     for col in col_dict:
+      #        col_dict[col] = find_table(col)
+      #     return(col_dict)
+      #
+      #
+      
+
+      
+      # are ranked_col and ranking_col in the same file?
+      return []
+   
