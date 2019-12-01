21092ef3ea75e66b6060473b26922f344ff560e5
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2019-11-30 23:47:43 -0500

    Add files via upload

---
+++ scripts/rasa_run_server.ps1
@@ -0,0 +1,2 @@
+﻿cd C:\personal\chatbot_july_2019\filebot_6
+rasa run --credentials credentials.yml
---
+++ scripts/rasa_run_tester_python_run.ps1
@@ -0,0 +1,4 @@
+﻿cd C:\personal\chatbot_july_2019\filebot_6
+rasa train
+rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials_test.yml
+rasa run actions
