121d6697f95c9e37f2a0364d6d37589e1f2d9e4b
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2020-01-25 14:21:52 -0500

    Add files via upload

--- actions.py
+++ actions.py
@@ -1110,7 +1110,8 @@ class action_show_details(Action):
                #poster_file = df_dict['movies'][df_dict['movies']['original_title'].str.lower()==slot_dict['movie'].lower()]['poster_path']
             logging.warning("poster_file is "+str(poster_file.iloc[0]))
             # TODO for test need URL of this form http://127.0.0.1:5000/rhIRbceoE9lR4veEXuwCC2wARtG.jpg
-            target_URL = wv_URL+str(poster_file.iloc[0])
+            # target_URL = wv_URL+str(poster_file.iloc[0])
+            target_URL = http://52.168.9.70/
             logging.warning("target_URL is "+str(target_URL))
             message1 = {
                "attachment": {
