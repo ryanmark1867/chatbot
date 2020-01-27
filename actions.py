d582ba716ef6719c25f0bb58306805a544cf8eda
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2020-01-26 21:24:53 -0500

    Add files via upload

--- actions.py
+++ actions.py
@@ -1123,7 +1123,7 @@ class action_show_details(Action):
             # TODO for test need URL of this form http://127.0.0.1:5000/rhIRbceoE9lR4veEXuwCC2wARtG.jpg
             # target_URL = wv_URL+str(poster_file.iloc[0])
             #target_URL = "http://52.168.9.70"
-            target_URL = "https://cbc.ca"
+            target_URL = "https://52.168.9.70:5000/"
             logging.warning("target_URL is "+str(target_URL))
             message1 = {
                "attachment": {
