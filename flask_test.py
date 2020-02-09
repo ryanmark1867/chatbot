b487859584b443d4d8a62add8867c86e2caeebe4
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2020-02-09 11:37:58 -0500

    Add files via upload

--- flask_test.py
+++ flask_test.py
@@ -23,9 +23,71 @@ import json
 
 # API_ENDPOINT = "http://localhost:5005/webhooks/rest/webhook"
 API_ENDPOINT = "http://localhost:5005/webhooks/rest/webhook"
+messagePayload = ''
+selected_item = ''
+selected_category = ''
+
+# BEGIN END STUFF
+'''
+import traceback
+from werkzeug.wsgi import ClosingIterator
+
+class AfterResponse:
+    def __init__(self, app=None):
+        self.callbacks = []
+        if app:
+            self.init_app(app)
+
+    def __call__(self, callback):
+        self.callbacks.append(callback)
+        return callback
+
+    def init_app(self, app):
+        # install extension
+        app.after_response = self
+
+        # install middleware
+        app.wsgi_app = AfterResponseMiddleware(app.wsgi_app, self)
+
+    def flush(self):
+        for fn in self.callbacks:
+            try:
+                fn()
+            except Exception:
+                traceback.print_exc()
+
+class AfterResponseMiddleware:
+    def __init__(self, application, after_response_ext):
+        self.application = application
+        self.after_response_ext = after_response_ext
+
+    def __call__(self, environ, after_response):
+        iterator = self.application(environ, after_response)
+        try:
+            return ClosingIterator(iterator, [self.after_response_ext.flush])
+        except Exception:
+            traceback.print_exc()
+            return iterator
+            
 
 
+app = Flask("after_response")
+AfterResponse(app)
 
+# post close actions 
+
+@app.after_response
+def payload_to_rasa():
+    
+    print("mpayload in payload_to_rasa is "+str(messagePayload))
+    print("selected_item is"+str(selected_item))
+    print("selected_category is"+str(selected_category))
+    r = requests.post(url = API_ENDPOINT, data = messagePayload)
+
+'''
+# END APP STUFF
+
+# uncomment this to get back
 app = Flask(__name__)
 
 HTML_TEMPLATE = Template("""
@@ -50,6 +112,9 @@ def package_list(key_name,list_in):
 def homepage():
     #global wv_payload
     # TODO REPLACE THIS HACKY WAY TO GET PAYLOAD
+    global messagePayload
+    global selected_item
+    global selected_category
     print("about to try to show page")
     wv_payload_path = 'wv_payload.pkl'
     with open(wv_payload_path, 'rb') as handle:
@@ -89,12 +154,11 @@ def homepage():
     print("mpayload is "+m_payload)
     messagePayloadPython = {"sender": "default","message": m_payload}
     messagePayload = json.dumps(messagePayloadPython)
-
-
-    
+ 
 
     return render_template('home.html',title=title,year = year,plot=plot,run_time=run_time,rating=rating,poster_url=poster_url,genre_list=genre_list,actor_list=actor_list, director_list=director_list)
-    r = requests.post(url = API_ENDPOINT, data = messagePayload) 
+    print("post return in main func")
+    # r = requests.post(url = API_ENDPOINT, data = messagePayload) 
     #return """<h1>Test of dynamic poster display here Feb 1 afternoon</h1>"""
     
 @app.route('/about/')
