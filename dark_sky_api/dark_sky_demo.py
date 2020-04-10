f54c4d72a8a204d0fb5c0ad8d993ab7e40bc2c88
Mark Ryan <39224115+ryanmark1867@users.noreply.github.com>
2020-04-10 17:35:51 -0400

    Add files via upload

--- dark_sky_api/dark_sky_demo.py
+++ dark_sky_api/dark_sky_demo.py
@@ -1,15 +1,5 @@
 # example of getting header data from dark-sky API
-'''
-import requests
 
-url = "https://dark-sky.p.rapidapi.com/43.653225,-79.383186,2007-03-01T01%253A32%253A33" 
-
-headers = {'x-rapidapi-host': "dark-sky.p.rapidapi.com",'x-rapidapi-key': "1223a453a2msh3c2958dc3570493p162d2ejsn0b5b23093bf3"}
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
-'''
 
 import requests
 
